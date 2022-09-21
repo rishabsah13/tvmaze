c43b4ad69bc7902cec97ff4a2c4929221bc58014
rishabsah13 <106971462+rishabsah13@users.noreply.github.com>
2022-09-21 18:46:33 +0530

    Add files via upload

---
+++ react-tvmaze/src/components/header/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import logo from "../../img/logo.png";
+
+const Header = () => {
+  return (
+    <header className="center">
+    </header>
+  );
+};
+
+export default Header;
